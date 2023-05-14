dd89fd3babb8c63b8df8ca333431ad4dcb1ba52d
reallyslimy <jesusvelezmar@gmail.com>
2023-05-14 13:18:16 +0200

    fix(env): leaking .env file

--- packages/spectrum-commands/src/utils/utils/LoadFiles.ts
+++ packages/spectrum-commands/src/utils/utils/LoadFiles.ts
@@ -1,27 +1,27 @@
-import glob from 'fast-glob';
-import { existsSync } from 'fs';
+import glob from 'fast-glob'
+import { existsSync } from 'fs'
 
 export async function loadFiles(dirName: string): Promise<string[]> {
-  const botFolder = `${process.cwd().replace(/\\/g, '/')}/fae`;
+  const botFolder = `${process.cwd().replace(/\\/g, '/')}/fae`
   const files = await glob(
     existsSync(botFolder)
       ? `${process.cwd().replace(/\\/g, '/')}/fae/src/${dirName}/**/*.{js,ts}`
       : `${process.cwd().replace(/\\/g, '/')}/src/${dirName}/**/*.{js,ts}`
-  );
+  )
 
   for (const file of files) {
-    delete require.cache[require.resolve(file)];
+    delete require.cache[require.resolve(file)]
   }
 
-  return files;
+  return files
 }
 
 export async function loadInternalFiles(dirName: string): Promise<string[]> {
-  const files = await glob(`${__dirname}/../../${dirName}/events/**/*.{js,ts}`);
+  const files = await glob(`${__dirname}/../../${dirName}/events/**/*.{js,ts}`)
 
   for (const file of files) {
-    delete require.cache[require.resolve(file)];
+    delete require.cache[require.resolve(file)]
   }
 
-  return files;
+  return files
 }
